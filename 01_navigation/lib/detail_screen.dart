324f3631eab446df1d57a0c7bb33a50e03c74b7b
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-06-23 12:18:37 +0900

    폴더 이름 수정

--- 01_navigation/lib/detail_screen.dart
+++ 01_navigation/lib/detail_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   const DetailScreen({super.key});
 
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
