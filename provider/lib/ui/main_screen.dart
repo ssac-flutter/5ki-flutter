8b2684d61fe8d5b09971363c2df6d9768bce84f5
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-06-29 15:26:08 +0900

    카운터 Provider 적용 버전

--- provider/lib/ui/main_screen.dart
+++ provider/lib/ui/main_screen.dart
@@ -1,34 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_exam/ui/main_view_model.dart';
 
-class MainScreen extends StatefulWidget {
+class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  int count = 0;
-
-  @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<MainViewModel>();
     return Scaffold(
       appBar: AppBar(
         title: const Text('카운터'),
       ),
       body: Center(
         child: Text(
-          '$count',
+          '${viewModel.count}',
           style: Theme.of(context).textTheme.displayLarge,
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState(() {
-            count++;
-          });
+          viewModel.increment();
         },
-        child: Text('+'),
+        child: const Text('+'),
       ),
     );
   }
