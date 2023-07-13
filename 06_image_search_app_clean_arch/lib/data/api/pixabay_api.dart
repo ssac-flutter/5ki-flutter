dbcdfae977cf74ab8c8c165d6a73d42f6033f671
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-07-13 12:25:25 +0900

    [websocket] first commit

--- 06_image_search_app_clean_arch/lib/data/api/pixabay_api.dart
+++ 06_image_search_app_clean_arch/lib/data/api/pixabay_api.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_search_app/data/dto/pixabay_result_dto.dart';
 
---
+++ websocket_sample/android/app/src/main/kotlin/com/example/websocket_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.websocket_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
