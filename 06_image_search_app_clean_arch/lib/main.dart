d5f694f42e602318701bd47a6094cf0c09aff95e
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-08-02 13:33:11 +0900

    github pages 배포

--- 06_image_search_app_clean_arch/lib/main.dart
+++ 06_image_search_app_clean_arch/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:image_search_app/core/router/router.dart';
 
@@ -12,6 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      scrollBehavior: const MaterialScrollBehavior().copyWith(
+        dragDevices: {
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.touch,
+          PointerDeviceKind.stylus,
+          PointerDeviceKind.unknown,
+        },
+      ),
       routerConfig: router,
       title: 'Flutter Demo',
       theme: ThemeData(
