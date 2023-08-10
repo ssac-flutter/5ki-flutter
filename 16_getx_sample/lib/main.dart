a64519224fedb65846a789355bea14358aad8a5e
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-08-10 11:45:43 +0900

    [GetX] GetX 샘플

---
+++ 16_getx_sample/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_sample/domain/counter_repository.dart';
+import 'package:getx_sample/presentation/main_screen.dart';
+
+import 'data/counter.dart';
+import 'presentation/main_view_model.dart';
+
+void main() {
+  final counter = Get.put(Counter());
+  Get.put(CounterRepository(counter));
+  Get.put(MainViewModel());
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
