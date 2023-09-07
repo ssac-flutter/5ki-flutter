68d6801bb7be7907ee2ee7a8eaa65b0d986722f0
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-09-07 17:39:18 +0900

    [타다 클론] 세로모드 고정

--- 23_tada_clone/lib/main.dart
+++ 23_tada_clone/lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:tada_clone/main/main_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]);
 
   await dotenv.load();
   final naverMapClientId = dotenv.env['NAVER_MAP_CLIENT_ID'];
